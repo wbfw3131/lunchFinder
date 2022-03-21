67e7c3b001d89a0be23d4463e57fbb070c371d72
wbfw3131 <81532443+wbfw3131@users.noreply.github.com>
2022-03-21 16:23:16 +0000

    Swtiched main back to checking Provo High

--- lunchfinder.py
+++ lunchfinder.py
@@ -114,4 +114,4 @@ def getSchoolCodes(schoolName: str) -> (str, str):
 if __name__ == "__main__":
     #print(dayLunch(day = "3/4/2022", school="Dixon Middle"))
     #print(dayLunch("7/4/2022"))
-    print(dayLunch(school="Dixon Middle"))
+    print(dayLunch())
