cdcb4cf2292dd6b662a4bdc24276042293d79648
wbfw3131 <wbfw3131@users.noreply.github.com>
2023-01-26 20:10:52 -0700

    Better school initialize, added diff. districts

--- school.py
+++ school.py
@@ -1,26 +1,35 @@
 import json, os
+from enum import Enum
 
 class School:
     def __init__(self, schoolName: str):
         schoolName = str(schoolName).strip().title()
         f = open(os.path.join(os.path.dirname(__file__), "schools.json"))
         table = json.load(f)
-        name = None
-        code1 = None
-        code2 = None
-        imageURL = None 
-        for term in table["schools"]:
-            # if term["name"].find(schoolName) != -1 or (term["name"] + " School").find(schoolName) != -1 or term["name"].find(schoolName.split(" ")[0]) != -1: #really only the last statement is needed since it'll be true if the others are, but keeping them here for now
-            if term["name"].find(schoolName.split(" ")[0]) != -1: #if other schools that have Provo in the name are added, this should be changed
-                name = term["name"]
-                code1 = term["siteCode1"]
-                code2 = term["siteCode2"]
-                imageURL = term["iconURL"]
         f.close()
-        if code1 == None:
-            raise ValueError("Can't find the specified school")
-        else:
-            self.name = name
-            self.code1 = code1
-            self.code2 = code2
-            self.imageURL = imageURL
+        # for district in table.values():
+        # for district in table.items():
+        for district in table.keys():
+            for term in table[district]:
+                # if term["name"].find(schoolName) != -1 or (term["name"] + " School").find(schoolName) != -1 or term["name"].find(schoolName.split(" ")[0]) != -1: #really only the last statement is needed since it'll be true if the others are, but keeping them here for now
+                if term["name"].find(schoolName.split(" ")[0]) != -1: #if other schools that have Provo in the name are added, this should be changed
+                    for attribute in term:
+                        self.__setattr__(attribute, term[attribute])
+                    self.__setattr__("district", district)
+                    
+                    
+                    # name = term["name"]
+                    # code1 = term["siteCode1"]
+                    # code2 = term["siteCode2"]
+                    # imageURL = term["iconURL"]
+                    return
+        
+
+        raise ValueError("Can't find the specified school")
+
+
+        
+#TODO figure out if I want to use an enum or just normal strings
+class District(Enum):
+    PROVO = "Provo"
+    ALPINE = "Alpine"
